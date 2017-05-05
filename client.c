ea9daae0117138aeaff098fd501d0287568208d9
public <public@palmela.netscdeec.ist.utl.pt>
2017-05-05 11:09:03 +0100

    sockets peer e client

--- client.c
+++ client.c
@@ -14,16 +14,45 @@ void checkError(int var, char * description){
 }
 
 int main(){
-	
-	int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
-	checkError(sock_fd, "socket");
-	
-	int err = bind(sock_fd, (struct sockaddr *)&local_addr, sizeof(local_addr));
-	checkError(err, "bind");
-	
+	int sock_fd_clientudp, sock_fd_clienttcp, err;
+
+	//INICIALIZA SOCKET CLIENT UDP
+	sock_fd_clientudp = socket(AF_INET, SOCK_DGRAM, 0);
+	checkError(sock_fd_clientudp, "socket");	
+
+	//INICIALIZA SOCKET CLIENT TCP
+	sock_fd_clienttcp = socket(AF_INET, SOCK_DGRAM, 0); 
+	checkError(sock_fd_client, "socket");
 	
 
+	// Bind socket para clients UDP
+	local_addr.sin_family = AF_INET; // Definições da socket UDP
+	local_addr.sin_port= htons(GATEWAY_PORT_CLIENTS);
+	local_addr.sin_addr.s_addr= INADDR_ANY;	
+	err = bind(sock_fd_clientudp, (struct sockaddr *)&local_addr, sizeof(local_addr));
+	checkError(err, "bind");
+
+	// Bind socket para clientes TCP
+	local_addr.sin_family = AF_INET;
+	local_addr.sin_port= htons(GATEWAY_PORT_PEERS);
+	local_addr.sin_addr.s_addr= INADDR_ANY;
+	err = bind(sock_fd_clienttcp, (struct sockaddr *)&local_addr, sizeof(local_addr));
+	checkError(err, "bind");
+
+
+	//ENVIA PARA GATEWAY
+	sendto(sock_fd_clientudp, buffer, sizeof(GatewayMsg), 0, (struct sockaddr *)&local_addr, sizeof(local_addr));
 	
+	//RECEBE DA GATEWAY
+	recv(sock_fd_clienttcp, buffer, sizeof(GatewayMsg), 0, (struct sockaddr *)&local_addr, sizeof(local_addr));
 	
+//    CONNECT
+//    SEND/RECV
+
+	//Close
+	close(sock_fd_clientudp);
+	close(sock_fd_clienttcp);
+
+
 	return 0;
 }
--- peer.c
+++ peer.c
@@ -9,6 +9,51 @@
 #include "sock_dg.h"
 
 int main(){
+
+	int sock_fd_peerudp, sock_fd_peertcp, err;
+
+	//INICIALIZA SOCKET PEER UDP	
+	sock_fd_peerudp = socket(AF_INET, SOCK_DGRAM, 0);
+	checkError(sock_fd_peerudp, "socket");
+		
+
+	//INICIALIZA SOCKET PEER TCP
+	sock_fd_peertcp = socket(AF_INET, SOCK_DGRAM, 0); 
+	checkError(sock_fd_peertcp, "socket");
+
 	
+	// Bind socket para peer UDP
+	local_addr.sin_family = AF_INET; // Definições da socket UDP
+	local_addr.sin_port= htons(GATEWAY_PORT_CLIENTS);
+	local_addr.sin_addr.s_addr= INADDR_ANY;	
+	err = bind(sock_fd_peerudp, (struct sockaddr *)&local_addr, sizeof(local_addr));
+	checkError(err, "bind");
+
+	// Bind socket para peer TCP
+	local_addr.sin_family = AF_INET;
+	local_addr.sin_port= htons(GATEWAY_PORT_PEERS);
+	local_addr.sin_addr.s_addr= INADDR_ANY;
+	err = bind(sock_fd_peertcp, (struct sockaddr *)&local_addr, sizeof(local_addr));
+	checkError(err, "bind");
+
+//UDP
+	//ENVIA PARA GATEWAY
+	sendto(sock_fd_peerudp, buffer, sizeof(GatewayMsg), 0, (struct sockaddr *)&local_addr, sizeof(local_addr));
+	//RETURN E SUPOSTAMENTE ACABA AQUI A UDP
+
+	
+//TCP
+//   Listen
+//   Accept
+//   Recv/send
+
+
+
+	//Close
+	close(sock_fd_peerudp);
+
+	close(sock_fd_peertcp); //la so diz pthread_exit
+
+
 	return 0;
 }
